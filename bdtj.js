301e9877df9aac469792b7c93d5ac8db40a04766
liuliangzheng <37011868+liuliang520500@users.noreply.github.com>
2022-04-23 17:15:23 +0800

    Delete bdtj.js

--- bdtj.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?20b76fce632dc35a49456b9e7f8e1032";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
